f69c50febc2222612109c10c6d7359d0008aa152
SidArtDesignLab <mmaheshverma1@gmail.com>
2026-01-01 14:04:06 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "valuemirror-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
